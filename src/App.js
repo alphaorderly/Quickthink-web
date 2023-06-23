a0773937e399f284c719a5ddc4a59a3ff3999801
nexus jelly <ilov1112@gmail.com>
2023-06-24 00:11:23 +0900

    Quickthink Initialization

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import User from './state/User';
+import Login from './screen/login/Login';
+import Main from './screen/main/Main';
+import CallBack from './Callback';
+import './App.css';
+
+const App = () => {
+
+  const [user, setUser] = useRecoilState(User);
+
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route 
+            exact
+            path='/callback'
+            element={
+              <CallBack />
+            }
+          />
+          <Route
+            exact
+            path='/'           
+            element={
+              user.token.length === 0
+              ?
+              <Navigate replace to={"/login"} />
+              :
+              <Navigate replace to={"/main"} />
+            }
+          />
+          <Route
+            path='/main/*'
+            element={
+              user.token.length === 0
+              ?
+              <Navigate replace to={"/login"} />
+              :
+              <Main />
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              user.token.length === 0
+              ?
+              <Login />
+              :
+              <Navigate replace to={"/main"} />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
